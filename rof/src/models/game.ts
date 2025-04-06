16ee7318280a7d94347ed50e8189031efd471140
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-04-06 18:34:18 +0200

    added different player names

--- rof/src/models/game.ts
+++ rof/src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = ['Patricia', 'Emilia'];
+    public players: string[] = ['Patricia', 'Emilia', 'Alicia', 'Ama'];
     public stack: string[] = [];
     public playedCard: string[] = [];
     public currentPlayer: number = 0;
