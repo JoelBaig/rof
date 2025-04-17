faabcef7fac45a03ba42257c0a539e688e6d8629
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-04-17 21:19:09 +0200

    added openDialog function

--- rof/src/app/game/game.component.ts
+++ rof/src/app/game/game.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../../models/game';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-game',
@@ -10,6 +19,10 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string | undefined;
   game: Game = new Game();
+
+  constructor(public dialog: MatDialog) {
+
+  }
 
   ngOnInit(): void {
     this.newGame();
@@ -31,5 +44,18 @@ export class GameComponent implements OnInit {
         this.game.playedCard.push(card);
       }, 1000);
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: {name: this.name(), animal: this.animal()},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if (result !== undefined) {
+        this.animal.set(result);
+      }
+    });
   }
 }
