3fd5c5f89112be350de62386b3da093bd93b4469
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-04-22 18:03:18 +0200

    added current player active boolean

--- rof/src/app/player/player.component.ts
+++ rof/src/app/player/player.component.ts
@@ -8,12 +8,13 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PlayerComponent implements OnInit {
 
   @Input() name: string = '';
+  @Input() playerActive: boolean = false;
 
   constructor() {
 
   }
 
   ngOnInit(): void {
-      
+
   }
 }
