e0e1eec7eaaea3d74d04c92d3fb7003ed140c7cc
JoelBaig <131587984+Joe482@users.noreply.github.com>
2025-04-22 19:16:18 +0200

    added game info card text

--- rof/src/app/app.module.ts
+++ rof/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { GameInfoComponent } from './game-info/game-info.component';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +31,8 @@ import { GameInfoComponent } from './game-info/game-info.component';
     MatIconModule,
     MatDialogModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
